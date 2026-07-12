427ca4c46801d5eb8d4eb2c307b088ad3027b6b9
nsmun <sn9943389@gmail.com>
2026-07-12 13:43:20 +0100

    added timeline

--- app/__init__.py
+++ app/__init__.py
@@ -271,3 +271,7 @@ def delete_time_line_post(post_id):
 		return {'error': 'Post not found'}, 404
 	post.delete_instance()
 	return {'deleted': post_id}
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
